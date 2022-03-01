d27ae9034d90c859969ee7f2972f30803c5a3ceb
ayushsingla <ayushsingla991@gmail.com>
2022-03-01 15:02:29 +0530

    new button sprites. uipos code updated.

--- Assets/_Scripts/UIPos.cs
+++ Assets/_Scripts/UIPos.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIPos : MonoBehaviour {
@@ -9,23 +10,22 @@ public class UIPos : MonoBehaviour {
     [SerializeField] Vector3 won;
     [SerializeField] Vector3 lose;
 
+    Dictionary<GameState, Vector3> posDict;
+
     RectTransform rect;
 
     void Start() {
+        posDict = new Dictionary<GameState, Vector3>();
+        posDict.Add(GameState.Idle, idle);
+        posDict.Add(GameState.Tutorial, tutorial);
+        posDict.Add(GameState.Start, start);
+        posDict.Add(GameState.Win, won);
+        posDict.Add(GameState.Lose, lose);
+
         rect = GetComponent<RectTransform>();
         rect.anchoredPosition = idle;
         GameManager.AddOnGameStateChanged(_gameState => {
-            if (_gameState == GameState.Idle) {
-                rect.DOAnchorPos(idle, UIManager.ANIM_DUR);
-            } else if (_gameState == GameState.Tutorial) {
-                rect.DOAnchorPos(tutorial, UIManager.ANIM_DUR);
-            } else if (_gameState == GameState.Start) {
-                rect.DOAnchorPos(start, UIManager.ANIM_DUR);
-            } else if (_gameState == GameState.Win) {
-                rect.DOAnchorPos(won, UIManager.ANIM_DUR);
-            } else if (_gameState == GameState.Lose) {
-                rect.DOAnchorPos(lose, UIManager.ANIM_DUR);
-            }
+            rect.DOAnchorPos(posDict[_gameState], UIManager.ANIM_DUR);
         });
     }
 }
