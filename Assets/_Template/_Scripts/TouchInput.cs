cec00ae1b0201bfb41885caff7381caebc67c9f2
ayushsingla <ayushsingla991@gmail.com>
2023-03-14 14:27:20 +0530

    added drag in touch input

--- Assets/_Template/_Scripts/TouchInput.cs
+++ Assets/_Template/_Scripts/TouchInput.cs
@@ -7,7 +7,11 @@ public class TouchInput : MonoBehaviour {
     bool isPressed;
 
     System.Action<bool> OnTouch;
+    System.Action<bool, Vector2> OnTouchPos;
     System.Action OnTouchHold;
+    System.Action<Vector2> OnDrag;
+
+    Vector2 lastMousePos;
 
     void Awake() {
         instance = this;
@@ -19,20 +23,21 @@ public class TouchInput : MonoBehaviour {
         }
         if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetKeyDown(KeyCode.Space))) {
             isPressed = true;
-            if (OnTouch != null) {
-                OnTouch(true);
-            }
+            lastMousePos = Input.mousePosition;
+            OnTouchPos?.Invoke(true, lastMousePos);
+            OnTouch?.Invoke(true);
         }
         if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetKeyUp(KeyCode.Space))) {
             isPressed = false;
-            if (OnTouch != null) {
-                OnTouch(false);
-            }
+            OnTouchPos?.Invoke(false, Vector2.zero);
+            OnTouch?.Invoke(false);
         }
         if (isPressed) {
-            if (OnTouchHold != null) {
-                OnTouchHold();
-            }
+            Vector2 mousePos = Input.mousePosition;
+            OnTouchHold?.Invoke();
+            Vector2 diff = mousePos - lastMousePos;
+            OnDrag?.Invoke(diff);
+            lastMousePos = mousePos;
         }
     }
 
@@ -40,8 +45,16 @@ public class TouchInput : MonoBehaviour {
         instance.OnTouch += _OnTouch;
     }
 
+    public static void AddOnTouch(System.Action<bool, Vector2> _OnTouch) {
+        instance.OnTouchPos += _OnTouch;
+    }
+
     public static void AddOnTouchHold(System.Action _OnTouchHold) {
         instance.OnTouchHold += _OnTouchHold;
+    }
+
+    public static void AddOnDrag(System.Action<Vector2> _OnDrag) {
+        instance.OnDrag += _OnDrag;
     }
 
 }
